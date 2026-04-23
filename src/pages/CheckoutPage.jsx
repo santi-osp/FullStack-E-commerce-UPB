86bc0e33123f93c60355af400677f9c1f4482d59
santi-osp <santioa2014@gmail.com>
2026-04-23 16:43:23 -0500

    feat: add CheckoutPage

---
+++ src/pages/CheckoutPage.jsx
@@ -0,0 +1,10 @@
+import CheckoutForm from '../components/organisms/CheckoutForm.jsx'
+
+export default function CheckoutPage() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-6">
+      <h1 className="mb-4 text-2xl font-bold text-white">Checkout</h1>
+      <CheckoutForm />
+    </div>
+  )
+}
